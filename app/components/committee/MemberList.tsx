8abf13408fd35a53eceba4419a721cc1f48f2443
Noa-hS <saysokhour@gmail.com>
2025-04-10 23:20:34 +1000

    Facelift + New data

--- app/components/committee/MemberList.tsx
+++ app/components/committee/MemberList.tsx
@@ -2,13 +2,13 @@ import MemberListItem from '@/app/components/committee/MemberListItem'
 
 // The default role is set to '<Team name> Officer', so don't need to set it in members.js
 
-export default function MemberList ({ teams }: { teams: { name: string, members: { name: string, image?: string }[] }[] }) {
+export default function MemberList({ teams }: { teams: { name: string, members: { name: string, image?: string }[] }[] }) {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-1 gap-x-10 gap-y-20 sm:gap-y-10">
+    <div className="grid grid-cols-1 gap-x-10 gap-y-20 justify-center sm:gap-y-10">
       {teams.map((team, teamIndex) => (
-        <div className="flex flex-col gap-5" key={teamIndex}>
+        <div className="flex flex-col justify-center gap-5" key={teamIndex}>
           <h4 className="text-xl text-text-secondary">{team.name}</h4>
-          <div className="grid grid-cols-2 sm:grid-cols-1 gap-2">
+          <div className="grid grid-cols-4 lg:grid-cols-2 justify-center sm:grid-cols-1 gap-4">
             {team.members.map((profile, profileIndex) => (
               <MemberListItem role={`${team.name} Officer`} {...profile} key={profileIndex} />
             ))}
