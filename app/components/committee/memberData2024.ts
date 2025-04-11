60ef8e6b337e6dd0c105f38a23ad71fc740987e2
Noa-hS <saysokhour@gmail.com>
2025-04-12 02:03:07 +1000

    photo updates #1

---
+++ app/components/committee/memberData2024.ts
@@ -0,0 +1,52 @@
+// Use the 'filter' property to set Tailwind css filters on images
+
+export const executives: { name: string, role: string, image?: string, filter?: string }[] = [
+  {
+    name: '',
+    role: '',
+    image: '',
+    filter: '',
+  },
+];
+
+export const directors: { name: string, role: string, image?: string, filter?: string }[] = [
+  {
+    name: '',
+    role: '',
+    image: '',
+    filter: '',
+  },
+];
+
+export const representatives: { name: string, role: string, image?: string, filter?: string }[] = [
+  {
+    name: '',
+    role: '',
+    image: '',
+    filter: '',
+  },
+];
+
+export const teams: {
+  name: string,
+  image?: string,
+  members: {
+    name: string,
+    role?: string,
+    image?: string,
+    filter?: string
+  }[]
+}[] = [
+  {
+    name: '',
+    image: '',
+    members: [
+      {
+        name: '',
+        role: '',
+        image: '',
+        filter: '',
+      },
+    ],
+  },
+];
