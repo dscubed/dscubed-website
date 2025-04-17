cf8c9e3dcaaf935d45a10e298834af9efc64807d
Noa-hS <saysokhour@gmail.com>
2025-04-17 15:09:34 +1000

    init placeholder

---
+++ app/components/home/Embeddings.tsx
@@ -0,0 +1,11 @@
+import Section from '@/app/components/Section'
+
+export default function Embeddings() {
+  return (
+    <Section> {/* Override the default vertical margins */}
+      <h2 className="text-5xl sm:text-4xl text-center leading-tight">
+        Placeholder
+      </h2>
+    </Section>
+  )
+}
