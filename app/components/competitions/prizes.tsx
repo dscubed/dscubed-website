ed3032d358833cca706c50d10111133e4da113c3
lachl8n <161278966+lachl8n@users.noreply.github.com>
2025-04-27 13:42:42 +1000

    added prize value animation

--- app/components/competitions/prizes.tsx
+++ app/components/competitions/prizes.tsx
@@ -1,5 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import Section from "@/app/components/Section";
+import { motion, useMotionValue, animate, useTransform } from "framer-motion";
+import { useEffect } from "react";
+
+// Function to animate the prize value adapted from Saleh Mubashar https://codesandbox.io/p/sandbox/floral-hill-np4qz5?file=%2Fsrc%2FApp.js&from-embed
+function executePrizeAnimation(value: number) {
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, Math.round);
+
+  useEffect(() => {
+    const animation = animate(count, value, {
+      duration: 2,
+    });
+
+    return animation.stop;
+  }, []);
+
+  return <motion.span>{rounded}</motion.span>;
+}
 
 export default function Prizes() {
   return (
@@ -8,7 +28,7 @@ export default function Prizes() {
         <div className="flex color-white relative h-96">
           <div className="text-center relative mx-5 top-1/4">
             <div className="my-0.3 mx-0.3 rounded-xl py-4 px-1 mb-3 bg-blue-700">
-              $50
+              ${executePrizeAnimation(50)}
             </div>
             <div className="my-0.3 mx-0.3 rounded-xl py-4 px-10 bg-zinc-800 py-0 px-1 text-0.8 h-3/4">
               2ND PRIZE
@@ -16,7 +36,7 @@ export default function Prizes() {
           </div>
           <div className="text-center relative mx-2">
             <div className="my-0.3 mx-0.3 rounded-xl py-4 px-10 mb-3 bg-blue-700">
-              $100
+              ${executePrizeAnimation(100)}
             </div>
             <div className="my-0.3 mx-0.3 rounded-xl py-4 px-10 bg-zinc-800 py-0 px-1 text-0.8 h-full">
               1ST PRIZE
@@ -24,7 +44,7 @@ export default function Prizes() {
           </div>
           <div className="text-center relative mx-5 top-1/2">
             <div className="my-0.3 mx-0.3 rounded-xl py-4 px-10 mb-3 bg-blue-700">
-              $25
+              ${executePrizeAnimation(25)}
             </div>
             <div className="my-0.3 mx-0.3 rounded-xl py-4 px-10 bg-zinc-800 py-0 px-1 text-0.8 h-1/2">
               3RD PRIZE
