dbf7d4fbebdfb82f6f9c116e276c1f7f3317378a
lachl8n <161278966+lachl8n@users.noreply.github.com>
2025-04-27 12:06:09 +1000

    full prizes design without heading or description, intend to copy from other components for consistency

--- app/competitions/page.tsx
+++ app/competitions/page.tsx
@@ -1,36 +1,41 @@
-import Footer from '@/app/components/Footer'
-import Navbar from '@/app/components/Navbar'
-import Section from '@/app/components/Section'
+import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Navbar";
+import Section from "@/app/components/Section";
+import Prizes from "@/app/components/competitions/prizes";
 
 export const metadata = {
-  title: 'Competitions | DSCubed',
-  description: 'Explore our competitions and challenges.',
+  title: "Competitions | DSCubed",
+  description: "Explore our competitions and challenges.",
   openGraph: {
-    title: 'Competitions | DSCubed',
-    description: 'Explore our competitions and challenges.',
-    url: '/competitions',
-    siteName: 'DSCubed',
-    locale: 'en_AU',
-    type: 'website',
+    title: "Competitions | DSCubed",
+    description: "Explore our competitions and challenges.",
+    url: "/competitions",
+    siteName: "DSCubed",
+    locale: "en_AU",
+    type: "website",
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title: "Competitions | DSCubed",
-    description: 'Explore our competitions and challenges.',
+    description: "Explore our competitions and challenges.",
   },
-}
+};
 
 export default function CompetitionsPage() {
   return (
     <>
       <Navbar />
 
-      <main className="flex items-center justify-center min-h-screen">
-        <h1 className="text-5xl font-bold text-center">Competitions</h1>
+      <main className="relative bg-background-secondary z-10">
+        <div className="text-center my-8">
+          <h1 className="text-2xl font-bold">Competitions</h1>
+        </div>
+        <hr className="border=0 border-b border-border" />
+        <Prizes />
       </main>
 
       <Footer />
       <p>Paul is awesome</p>
     </>
-  )
+  );
 }
