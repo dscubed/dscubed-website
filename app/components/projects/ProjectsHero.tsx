8d27d242e20ee464a666492acb07f52274ed0af6
tchanwangsa <141188429+Tchanwangsa@users.noreply.github.com>
2025-05-20 23:06:40 +1000

    Hero section

---
+++ app/components/projects/ProjectsHero.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Spline from "@splinetool/react-spline";
+
+export default function ProjectsHero() {
+  return (
+    <>
+      <style jsx global>{`
+        @font-face {
+          font-family: "Akira Expanded";
+          src: url("/fonts/Akira Expanded Demo.otf") format("opentype");
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+      `}</style>
+      <div className="w-screen h-screen relative">
+        <Spline scene="https://prod.spline.design/eQqi6a4D3kJA8Xri/scene.splinecode" />
+        <div
+          className="absolute top-0 left-0 w-full h-3/5 flex flex-col gap-4 items-center justify-end pointer-events-none"
+          style={{ fontFamily: "'Akira Expanded', sans-serif" }}
+        >
+          <h1 className="text-white text-6xl font-black">
+            PROJECTS INITIATIVE
+          </h1>
+          <p className="w-3/5 text-center">
+            THE DS3 STUDENT INITIATIVE IS A UNIQUE OPPORTUNITY FOR STUDENTS TO
+            LEVERAGE THE POWER OF DATA ANALYTICS TOOLS TO CREATE HIGH-IMPACT
+            RESEARCH PROJECTS THAT SOLVE REAL LIFE PROBLEMS
+          </p>
+          <button className="mt-3 px-6 py-2 rounded-2xl bg-white text-black text-lg font-extrabold shadow-lg hover:bg-cyan-100 transition-colors duration-200 pointer-events-auto">
+            MORE INFO
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
