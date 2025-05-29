2aadedd682be6b17a005bfb4e275793502a1c89a
Michael Ren <liyur@student.unimelb.edu.au>
2025-05-29 15:05:16 +1000

    Update frontpageembed.tsx

--- app/components/visualiser/frontpageembed.tsx
+++ app/components/visualiser/frontpageembed.tsx
@@ -16,12 +16,12 @@ export default function FrontPageEmbed() {
   // router.push("/visualiser");
 
   return (
-    <div className="relative w-full mt-[-63px] shadow-[0_0_200px_50px_rgba(20,15,30,1)] border-b-2 border-b-[rgb(44,44,54)]">
+    <div className="relative w-full mt-[-63px] shadow-[0_0_200px_50px_rgba(35,40,80,1)] border-b-2 border-b-[rgb(57,64,90)]">
       {/* Gradient background */}
       <div className="animated-gradient-1 absolute inset-0 w-full h-full opacity-50"></div>
 
       {/* Noise filter */}
-      <div className="noise-filter absolute inset-0 w-full h-full opacity-50 pointer-events-none z-10"></div>
+      <div className="noise-filter absolute inset-0 w-full h-full opacity-50 pointer-events-none z-20"></div>
       
       {/* Take up navbar space */}
       <div className="h-[63px]"></div>
