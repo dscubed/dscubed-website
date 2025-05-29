5326754c9f4111771a3144899e9bdb4bff8f009c
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-05-29 16:26:53 +1000

    Update nav links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -82,6 +82,8 @@ export default function Navbar ({ className = '', ...rest }: { className?: strin
               <div className="block h-1/2 w-0 border-l-2 border-black/10 dark:border-white/10 my-auto"></div>
               <Link className="my-auto" href="/events">Events</Link>
               <Link className="my-auto" href="/committee-2025">Committee</Link>
+              <Link className="my-auto" href="/competitions">Competition</Link>
+              <Link className="my-auto" href="/visualiser">Visualiser</Link>
               <Link className="my-auto" href="/sponsors">Sponsors</Link>
               <Link className="my-auto px-4 py-2 bg-foreground text-background rounded-full" href="https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/dscubed/" target="_blank">Membership</Link>
             </div>
@@ -98,8 +100,27 @@ export default function Navbar ({ className = '', ...rest }: { className?: strin
         <div className={clsx("absolute hidden flex-col w-full bg-background-secondary top-full border-t border-border", {
           'md:flex': showMenu,
         })}>
+          <Link className="p-4 border-b border-border flex items-center gap-4" href="/events">
+            <span>Discord</span>
+            <motion.div
+              className="w-6 h-6"
+              animate={{
+                rotate: [0, 360], // Full rotation
+                scale: [1, 1.1, 1], // Slight scaling effect
+              }}
+              transition={{
+                duration: 1, // Animation duration
+                repeat: Infinity, // Infinite loop
+                repeatDelay: 2, // 2 seconds delay between animations
+              }}
+            >
+              <img src="/competitions/discordicon.png" alt="Discord" className="w-6 h-6 saturate-0 brightness-200 contrast-150" />
+            </motion.div>
+          </Link>
           <Link className="p-4 border-b border-border" href="/events">Events</Link>
-          <Link className="p-4 border-b border-border" href="/committee">Committee</Link>
+          <Link className="p-4 border-b border-border" href="/committee-2025">Committee</Link>
+          <Link className="p-4 border-b border-border" href="/competitions">Competition</Link>
+          <Link className="p-4 border-b border-border" href="/visualiser">Visualiser</Link>
           <Link className="p-4 border-b border-border" href="/sponsors">Sponsors</Link>
           <Link className="p-4 border-b border-border" href="https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/dscubed/" target="_blank">Membership</Link>
           {/* <ThemeButton className="p-4 border-b-2 border-border" state={themeState} /> */}
