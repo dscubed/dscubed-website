f67ec734dc3a21720897284752d49f99e44a2e05
Michael Ren <liyur@student.unimelb.edu.au>
2025-05-29 11:22:39 +1000

    Fix event section styling

--- app/components/events/EventGallery.tsx
+++ app/components/events/EventGallery.tsx
@@ -1,26 +1,21 @@
-import { fetchEvents } from "@/app/lib/data";
-import EventCard from "@/app/components/events/EventCard";
+import { fetchEvents } from '@/app/lib/data'
+import EventCard from "@/app/components/events/EventCard"
 
-export default async function EventGallery({
-  range = [0, 3],
-}: {
-  range: [number, number];
-}) {
-  const events = await fetchEvents(range);
+export default async function EventGallery ({ range = [0, 3] }: { range: [number, number] }) {
+  const events = await fetchEvents(range)
 
   return (
-    <div className="grid grid-cols-4 lg:grid-cols-2 sm:grid-cols-1 gap-12 mx-auto">
+    <div className="grid grid-cols-4 lg:grid-cols-2 sm:grid-cols-1 grid-rows-[max-content] sm:mx-auto gap-4">
       {events.map((item, index) => (
-        <div key={index} className="transform scale-70">
-          <EventCard
-            title={item.title}
-            description={item.description}
-            thumbnail={item.thumbnail}
-            date={item.date}
-            link={item.link}
-          />
-        </div>
+        <EventCard
+          title={item.title}
+          description={item.description}
+          thumbnail={item.thumbnail}
+          date={item.date}
+          link={item.link}
+          key={index}
+        />
       ))}
     </div>
-  );
+  )
 }
--- app/components/home/EventSection.tsx
+++ app/components/home/EventSection.tsx
@@ -1,32 +1,23 @@
-import Link from "next/link";
-import Section from "@/app/components/Section";
-import EventGallery from "@/app/components/events/EventGallery";
-import { ArrowRightIcon } from "@heroicons/react/24/solid";
-import { Suspense } from "react";
-import EventGallerySkeleton from "../events/EventGallerySkeleton";
+import Link from 'next/link'
+import Section from '@/app/components/Section'
+import EventGallery from '@/app/components/events/EventGallery'
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
+import { Suspense } from 'react'
+import EventGallerySkeleton from '../events/EventGallerySkeleton'
 
-export default function EventSection() {
+export default function EventSection () {
   return (
     <Section>
-      <h2 className="text-5xl sm:text-4xl text-center mx-auto mb-6 leading-tight">
-        Recent Events
-      </h2>
+      <h2 className="text-5xl sm:text-4xl text-center mx-auto mb-5 leading-tight">Upcoming Events</h2>
 
-      <div className="flex justify-center py-4">
-        <Suspense fallback={<EventGallerySkeleton />}>
-          <EventGallery range={[0, 3]} />
-        </Suspense>
-      </div>
-
-      <div className="text-center mt-2">
-        <Link
-          className="flex justify-center items-center gap-2 text-3xl font-bold text-white mx-auto hover:underline"
-          href="/events"
-        >
-          <span>Browse All Events</span>
-          <ArrowRightIcon className="w-8 h-8" />
-        </Link>
-      </div>
+      <Suspense fallback={<EventGallerySkeleton />}>
+        <EventGallery range={[0, 3]} />
+      </Suspense>
+      
+      <Link className="flex gap-2 text-xl text-theme mx-auto" href="/events">
+        <span className="my-auto">Browse All Events</span>
+        <ArrowRightIcon className="w-6 h-6 my-auto" />
+      </Link>
     </Section>
-  );
+  )
 }
