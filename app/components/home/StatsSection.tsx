0b6f1fde99566109b483c3fa0de0cd9d15030bca
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-05-29 13:15:13 +1000

    Improve stats section styling

--- app/components/home/StatsSection.tsx
+++ app/components/home/StatsSection.tsx
@@ -4,69 +4,43 @@ import Section from "@/app/components/Section";
 // Statistics (currently taken from old AboutSection)
 const stats = [
   {
-    id: "members",
-    number: 123,
-    title: "Title?",
-    description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae sapien ac eros.",
+    amount: "550+",
+    description: "Number of current members in 2025.",
   },
   {
-    id: "degrees",
-    number: 123,
-    title: "Title?",
-    description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae sapien ac eros.",
+    amount: "2.1K",
+    description: "Instagram followers to date.",
   },
   {
-    id: "events",
-    number: 123,
-    title: "Title?",
-    description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae sapien ac eros.",
+    amount: "14",
+    description: "Lorem ipsum dolor sit amet consectetur.",
   },
 ];
 
 export default function StatsSection() {
-  const formatNumber = (num: number) => {
-    if (num < 10) {
-      return `00${num}`;
-    }
-    if (num < 100) {
-      return `0${num}`;
-    }
-    return `${num}`;
-  };
-
   return (
     <Section>
       <div className="grid grid-cols-[1fr,1fr] lg:grid-cols-1 lg:gap-20 sm:gap-10">
         <div>
-          <div className="sticky max-w-sm sm:max-w-none top-[calc(102.55px+20px)]">
-            <h2 className="text-5xl sm:text-4xl !leading-tight mb-10">
-              About Us
-            </h2>
-            <p className="text-text-secondary text-xl leading-relaxed">
-              DSCubed is dedicated to promoting data science, analytics, and
-              decision making skills across all disciplines.
+          <div className="sticky max-w-md sm:max-w-none top-[calc(102.55px+20px)] space-y-4">
+            <h2 className="text-5xl sm:text-4xl">Club Stats</h2>
+            <p className="text-text-secondary text-lg leading-relaxed">
+              DSCubed is dedicated to promoting data science, analytics, and decision making skills across all disciplines.
             </p>
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-0 max-w-xl lg:max-w-none">
+        <div className="grid grid-cols-1 gap-0 max-w-lg lg:max-w-none space-y-20 sm:space-y-10">
           {stats.map((stat, index) => (
-            <div key={stat.id}>
-              <div className="flex flex-col gap-4 rounded-md py-10">
-                <h3 className="text-[6rem] sm:text-[6rem] font-bold tracking-tight leading-none">
-                  {formatNumber(stat.number)}
+            <div key={index}>
+              <div className="space-y-1 rounded-md">
+                <h3 className="text-8xl sm:text-7xl tracking-tight leading-none">
+                  {stat.amount}
                 </h3>
-                <h4 className="text-xl">{stat.title}</h4>
                 <p className="text-xl lg:text-lg leading-relaxed">
                   {stat.description}
                 </p>
               </div>
-              {index < stats.length - 1 && (
-                <hr className="border-t-2 border-white border-opacity-40" />
-              )}
             </div>
           ))}
         </div>
