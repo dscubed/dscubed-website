6da13c985fcebeac373e5151c98db868b4668274
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-05-29 20:26:38 +1000

    Improve layout and page responsiveness

--- app/components/projects/ProjectsHero.tsx
+++ app/components/projects/ProjectsHero.tsx
@@ -6,6 +6,9 @@ import Carousel from "@/app/components/projects/carousel";
 import Section from "../Section";
 import ThemeButton from "../ThemeButton";
 import { describe } from "node:test";
+import Link from "next/link";
+import Image from "next/image";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 export default function ProjectsHero() {
   const [loading, setLoading] = useState(true);
@@ -13,15 +16,15 @@ export default function ProjectsHero() {
   const slideData = [
         {
       title: "Lung Cancer Classification",
-      description: "AI-based classification of lung cancer types from chest CT scans",
-      button: "More Info",
+      description: "Our project explores advanced deep learning techniques for lung cancer detection using medical CT scans. We focus on classifying the three main subtypes of non-small cell lung cancer—adenocarcinoma, squamous cell carcinoma, and large-cell carcinoma—by comparing the performance of Convolutional Neural Networks (CNNs) and Vision Transformers (ViTs). Utilizing both baseline and pre-trained models, including VGG-16 and a hybrid ViT, we preprocess and augment data to enhance model accuracy. Our work highlights the strengths of each approach in medical image analysis, aiming to improve diagnostic precision and support clinical decision-making through cutting-edge AI technology.",
+      button: "View Project",
       src: "/projects/xray.png",
       link: "/components/projects/cancer"
     },
     {
       title: "Stock Forecasting",
-      description: "Using machine learning models to predict stock prices and market trends",
-      button: "More Info",
+      description: "Our project explores advanced machine learning techniques for stock market prediction by analyzing historical financial data to forecast both price and trend movements. We compare the performance of various models—including traditional regressors and deep learning architectures—and ultimately develop an ensemble stacking model for enhanced predictive accuracy. Leveraging techniques such as feature engineering, time-series preprocessing, and hyperparameter tuning, we aim to build a robust system for automated investment decision-making. This work underscores the potential of AI in financial forecasting, offering insights that support smarter trading strategies and risk management in volatile markets.",
+      button: "View Project",
       src: "/projects/stock.png",
       link: "/components/projects/stock"
     },
@@ -40,55 +43,74 @@ export default function ProjectsHero() {
       `}</style>
 
       {/* Hero Section */}
-      <div className="w-screen h-screen relative">
-        <Spline
-          scene="https://prod.spline.design/eQqi6a4D3kJA8Xri/scene.splinecode"
-          onLoad={() => setLoading(false)}
-          onError={() => setLoading(false)}
-        />
+      <div className="w-screen h-[calc(100svh-63px)] lg:h-[calc(70svh-63px)] relative">
+        <div className="relative w-full h-full">
+          <Spline
+            scene="https://prod.spline.design/eQqi6a4D3kJA8Xri/scene.splinecode"
+            onLoad={() => setLoading(false)}
+            onError={() => setLoading(false)}
+          />
+        </div>
         {loading ? (
-          <div className="absolute inset-0 flex items-center justify-center z-10 bg-black/40">
-            <span className="text-white text-xl font-bold animate-pulse">
-              Loading...
-            </span>
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 bg-background-secondary text-text-primary text-xl font-bold animate-pulse">
+            Loading...
           </div>
         ) : (
-          <motion.div
-            initial={{ opacity: 0, y: 32 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 2, ease: "easeOut" }}
-            className="absolute top-0 left-0 w-full h-3/5 flex flex-col gap-4 items-center justify-end pointer-events-none"
-            style={{ fontFamily: "'Akira Expanded', sans-serif" }}
-          >
-            <h1 className="text-text-primary text-6xl font-black text-center">
-              PROJECTS INITIATIVE
-            </h1>
-            <p className="w-3/5 text-center text-text-primary">
-              THE DS3 STUDENT INITIATIVE IS A UNIQUE OPPORTUNITY FOR STUDENTS TO
-              LEVERAGE THE POWER OF DATA ANALYTICS TOOLS TO CREATE HIGH-IMPACT
-              RESEARCH PROJECTS THAT SOLVE REAL LIFE PROBLEMS
-            </p>
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-max px-4">
+            <motion.div
+              initial={{ opacity: 0, y: 32 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 2, ease: "easeOut" }}
+              className="flex flex-col gap-4 items-center justify-end pointer-events-none"
+              style={{ fontFamily: "'Akira Expanded', sans-serif" }}
+              >
+              <h1 className="text-text-primary text-6xl lg:text-5xl font-black text-center">
+                PROJECTS INITIATIVE
+              </h1>
+              <p className="max-w-3xl text-center text-text-primary">
+                THE DS3 STUDENT INITIATIVE IS A UNIQUE OPPORTUNITY FOR STUDENTS TO
+                LEVERAGE THE POWER OF DATA ANALYTICS TOOLS TO CREATE HIGH-IMPACT
+                RESEARCH PROJECTS THAT SOLVE REAL LIFE PROBLEMS
+              </p>
 
-            <button
-              className="mt-3 px-6 py-2 rounded-full bg-foreground text-black text-lg font-extrabold shadow-lg hover:bg-theme transition-colors duration-200 pointer-events-auto"
-              onClick={() => {
-                const nextSection = document.getElementById('carousel-section');
-                if (nextSection) {
-                  nextSection.scrollIntoView({ behavior: 'smooth' });
-                }
-              }}
-            >
-              explore projects
-            </button>
-          </motion.div>
+              <button
+                className="mt-3 px-6 py-2 rounded-full bg-foreground text-black text-lg font-extrabold shadow-lg hover:bg-theme transition-colors duration-200 pointer-events-auto"
+                onClick={() => {
+                  const nextSection = document.getElementById('project-gallery');
+                  if (nextSection) {
+                    nextSection.scrollIntoView({ behavior: 'smooth' });
+                  }
+                }}
+                >
+                explore projects
+              </button>
+            </motion.div>
+          </div>
         )}
       </div>
 
-      
-      <div id="carousel-section">
+      <div id="project-gallery">
         <Section>
           <h2 className="text-3xl font-semibold mb-6 text-center">Explore Our Projects</h2>
-          <Carousel slides={slideData} />
+          {/* <Carousel slides={slideData} /> */}
+
+          {/* Since there are only two projects, just use a flexbox */}
+          <div className="flex flex-wrap gap-8 justify-center">
+            {slideData.map((slide, index) => (
+              <div key={index} className="flex flex-col basis-1/2 grow-0 shrink-0 min-w-96 max-w-96 bg-background rounded-2xl overflow-hidden">
+                <Image src={slide.src} alt={slide.title} width={480} height={480} className="w-full h-48 object-cover" />
+                <div className="p-4 h-full flex-1 flex flex-col gap-4">
+                  <h3 className="text-xl font-semibold">{slide.title}</h3>
+                  <p className="text-text-secondary line-clamp-[8] min-h-[170px]">{slide.description}</p>
+                  <Link href={slide.link} className="flex justify-between items-center w-full mt-auto text-theme font-medium">
+                    <span>{slide.button}</span>
+                    <ArrowRightIcon className="w-5 h-5 stroke-[0.5] stroke-theme" />
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+
         </Section>
       </div>
     </>
