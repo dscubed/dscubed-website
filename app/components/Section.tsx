f4efe85ef3f1206bdeef02925947feb13caf6650
Michael Ren <liyur@student.unimelb.edu.au>
2025-05-30 16:20:01 +1000

    Update Section.tsx

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -1,15 +1,17 @@
 import clsx from 'clsx'
 
-export default function Section ({ 
+export default function Section ({
+  id,
   className, 
   children, 
   ...props 
 }: {
+  id?: string,
   className?: string,
   children?: React.ReactNode
 }) {
   return (
-    <section {...props} className={clsx('px-5 my-40 sm:my-24', className)}>
+    <section {...props} id={id} className={clsx('px-5 my-40 sm:my-24', className)}>
       <div className="flex flex-col gap-10 max-w-screen-xl lg:max-w-[500px] mx-auto">
         {children}
       </div>
